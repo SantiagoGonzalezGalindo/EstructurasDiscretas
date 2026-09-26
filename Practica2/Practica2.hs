43243de7b4c0498ee729fe3d7f3c9534b7bfcd65
SantiagoGonzalezGalindo <sdgoga1209@ciencias.unam.mx>
2026-09-26 04:04:13 -0600

    Adicion de funcion cashback a archivo Practica2.hs

--- Practica2/Practica2.hs
+++ Practica2/Practica2.hs
@@ -6,3 +6,14 @@
 
 reconversion :: Float -> Float
 reconversion x = (x / 1000 )
+
+{- 
+    Funcion : cashback
+    Descripcion: Calcula el cashback con el 10% de puntos, es lo mismo que dividir mi monto entre 10. Por lo que la funcion toma el monto y lo divide entre 10
+    Uso: cashback 2640 = 264.0
+-} 
+
+cashback :: Float -> Float
+cashback x = (x / 10)
+
+
